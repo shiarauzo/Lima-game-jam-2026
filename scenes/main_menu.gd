9ac6f120a46a58a766bf90455631ec9f4abf5df3
shiara <shiara.arauzo@gmail.com>
2026-01-31 01:41:03 -0500

    add buffer for music

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,14 +1,28 @@
 extends Control
 
+@onready var play_button = $VBoxContainer/Play
+@onready var options_modal = $OptionsModal
+
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_pressed)
 
 func _on_play_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://scenes/firstlevel.tscn")
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	options_modal.popup_centered()
 
+func _on_options_modal_close_requested() -> void:
+	options_modal.hide()
+
+func _on_master_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(value))
 
 func _on_close_pressed() -> void:
-	print("Close pressed")
 	get_tree().quit()
