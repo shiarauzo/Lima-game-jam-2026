17d111981b74fb917f5ae182bdeff58391c7d20f
shiara <shiara.arauzo@gmail.com>
2026-01-31 07:57:15 -0500

    add puzzle

--- scenes/secondlevel.gd
+++ scenes/secondlevel.gd
@@ -11,3 +11,6 @@ func _on_object3_pressed() -> void:
 
 func _on_object4_pressed() -> void:
 	$Object4.queue_free()
+
+func _on_go_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/thirdlevel.tscn")
