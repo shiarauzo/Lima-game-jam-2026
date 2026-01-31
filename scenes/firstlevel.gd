65f2df5f760274e67e9cd101b94ee56f4c02975d
shiara <shiara.arauzo@gmail.com>
2026-01-31 02:33:31 -0500

    add modal interaction

--- scenes/firstlevel.gd
+++ scenes/firstlevel.gd
@@ -3,7 +3,8 @@ extends Control
 @onready var card_modal = $CardModal
 
 func _on_card_pressed() -> void:
-	card_modal.popup_centered()
+	print("Card pressed")
+	card_modal.show()
 
 func _on_card_modal_close_requested() -> void:
 	card_modal.hide()
