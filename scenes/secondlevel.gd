0f683c9e07ffb5e4515c1cc261be6411a7c0afa8
shiara <shiara.arauzo@gmail.com>
2026-02-01 01:33:23 -0500

    add background and puzzle

--- scenes/secondlevel.gd
+++ scenes/secondlevel.gd
@@ -1,16 +1,1 @@
 extends Control
-
-func _on_object1_pressed() -> void:
-	$Object1.queue_free()
-
-func _on_object2_pressed() -> void:
-	$Object2.queue_free()
-
-func _on_object3_pressed() -> void:
-	$Object3.queue_free()
-
-func _on_object4_pressed() -> void:
-	$Object4.queue_free()
-
-func _on_go_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/thirdlevel.tscn")
